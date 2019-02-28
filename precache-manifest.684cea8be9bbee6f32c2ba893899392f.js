71ea0c50cd8498d266755c20da5dca013ab5dec0
filiplipinski <filip.lipinski@outlook.com>
2019-02-28 17:45:46 +0100

    Alpha v.1.0
    Just the begging of the site

---
+++ precache-manifest.684cea8be9bbee6f32c2ba893899392f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5dd8977e5fd3e346a1b43b0b66b4149",
+    "url": "/static/media/fontello.d5dd8977.woff2"
+  },
+  {
+    "revision": "05b3086bf7d813bd8d87",
+    "url": "/static/css/main.62f3a99f.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "69cf1a697659df6f4f38",
+    "url": "/static/js/2.69cf1a69.chunk.js"
+  },
+  {
+    "revision": "ff6086f40c252daef308c3bd293a9d77",
+    "url": "/static/media/js.ff6086f4.png"
+  },
+  {
+    "revision": "b64f346a75f0c02bffa701b87a561e30",
+    "url": "/static/media/thebestmovies.b64f346a.jpg"
+  },
+  {
+    "revision": "dc3e13843e367c39ecd04175c7a6c286",
+    "url": "/static/media/psdtohtml.dc3e1384.jpg"
+  },
+  {
+    "revision": "9caf3a416ea5eafe9ae037ce35a950d8",
+    "url": "/static/media/simple_todo.9caf3a41.jpg"
+  },
+  {
+    "revision": "7bb03661c6551e36ce9b6f1b5712bf52",
+    "url": "/static/media/fontello.7bb03661.eot"
+  },
+  {
+    "revision": "05b3086bf7d813bd8d87",
+    "url": "/static/js/main.05b3086b.chunk.js"
+  },
+  {
+    "revision": "3e5350478b91d61ab7097b0bc4b20216",
+    "url": "/static/media/fontello.3e535047.woff"
+  },
+  {
+    "revision": "fdeec51cb42f949061f68a47f2bc3955",
+    "url": "/static/media/fontello.fdeec51c.ttf"
+  },
+  {
+    "revision": "9b51fbf988e45d6c0ea1fa906757687f",
+    "url": "/static/media/fontello.9b51fbf9.svg"
+  },
+  {
+    "revision": "fe1806aa485d6646ae475038ae9804f3",
+    "url": "/static/media/hobby2.fe1806aa.jpg"
+  },
+  {
+    "revision": "ee5712a42d9505c3893a820e29dfc31c",
+    "url": "/static/media/hobby1.ee5712a4.jpg"
+  },
+  {
+    "revision": "4375acce29df6eff9a87c9785c711f04",
+    "url": "/static/media/hobby4.4375acce.jpg"
+  },
+  {
+    "revision": "fec796acaab79659fc468e9b44df1239",
+    "url": "/static/media/hobby3.fec796ac.jpg"
+  },
+  {
+    "revision": "8d0c89c2d0122ce12f7a961676dec9dd",
+    "url": "/index.html"
+  }
+];
